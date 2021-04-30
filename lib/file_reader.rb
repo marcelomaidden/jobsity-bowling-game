92b8d1dc1361dc5993c81cb5c067abdfb5dfaae3
Marcelo Fernandes de Araujo <marcelomaidden@gmail.com>
2021-04-30 18:27:36 -0300

    Adds error when is not possible to read file

--- lib/file_reader.rb
+++ lib/file_reader.rb
@@ -2,13 +2,12 @@
 
 # FileReader reads game file and stores player names and points in an array
 class FileReader
-  attr_reader :data
+  attr_reader :data, :error
 
   def initialize(filename)
     file = File.open(filename)
     @data = file.readlines.map(&:chomp)
-
-  rescue
-    @data = "File doesn't exist"
+  rescue Errno::ENOENT
+    @error = "File can't be read"
   end
 end
