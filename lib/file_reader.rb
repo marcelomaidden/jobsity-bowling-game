34bfee2531b70de916f5f5c2213a42ec2c31ffe1
Marcelo Fernandes de Araujo <marcelomaidden@gmail.com>
2021-04-30 19:43:35 -0300

    Handles empty file

--- lib/file_reader.rb
+++ lib/file_reader.rb
@@ -2,12 +2,27 @@
 
 # FileReader reads game file and stores player names and points in an array
 class FileReader
-  attr_reader :data, :error
+  attr_reader :error, :data
 
   def initialize(filename)
+    @data = {}
     file = File.open(filename)
-    @data = file.readlines.map(&:chomp)
+    @lines = file.readlines.map(&:chomp)
+    process_points
   rescue Errno::ENOENT
     @error = "File can't be read"
+  end
+
+  private
+
+  def process_points
+    @lines.each do |line|
+      user_throws = line.split(' ')
+      if @data[user_throws[0]]
+        @data[user_throws[0]].push(user_throws[1])
+      else
+        @data[user_throws[0]] = [user_throws[1]]
+      end
+    end
   end
 end
--- spec/file_reader_spec.rb
+++ spec/file_reader_spec.rb
@@ -1,14 +1,19 @@
 require_relative '../lib/file_reader'
 
-RSpec.describe 'Reads file' do
+RSpec.describe 'File reader' do
   let(:file) { FileReader.new('assets/game.txt') }
   let(:file2) { FileReader.new('assets/game2.txt') }
-
-  it 'Return an array of players and their throws' do
-    expect(file.data).to be_instance_of(Array)
-  end
+  let(:empty) { FileReader.new('empty.txt') }
 
   it "Returns an error message when the file doesn't exist" do
     expect(file2.error).to eq("File can't be read")
+  end
+
+  it 'Returns an Hash object with points for each player' do
+    expect(file.data).to be_instance_of(Hash)
+  end
+
+  it 'Returns an empty Hash if the file is empty' do
+    expect(empty.data).to be_instance_of(Hash)
   end
 end
