523007cb24520cc0e011d967512e85a27ba296c6
Marcelo Fernandes de Araujo <marcelomaidden@gmail.com>
2021-05-02 15:56:45 -0300

    Adds rainbow gem to colorize text

--- lib/game.rb
+++ lib/game.rb
@@ -1,4 +1,5 @@
 require 'terminal-table'
+require 'rainbow'
 
 class Game
   def initialize(data, score, pinfalls)
@@ -9,17 +10,17 @@ class Game
 
   def output
     table = Terminal::Table.new do |t|
-      t.add_row(['Frame', 1,2,3,4,5,6,7,8,9,10])
+      t.add_row([Rainbow('Frame').red, 1,2,3,4,5,6,7,8,9,10])
       t.add_separator
       @data.each_key do |player|
         player_score = @score.new(player, @pinfalls.new(@data[player]))
         next unless player_score.valid?
 
-        t.add_row([player_score.player])
+        t.add_row([Rainbow(player_score.player).blue.bright])
         t.add_separator
-        t.add_row(['Pinfalls'].concat(player_score.pinfalls.formatted))
+        t.add_row([Rainbow('Pinfalls').cyan].concat(player_score.pinfalls.formatted))
         t.add_separator
-        t.add_row(['Score'].concat(player_score.total))
+        t.add_row([Rainbow('Score').yellow].concat(player_score.total))
         t.add_separator
       end
     end
